a19aec31aaf3a6562b6d6b1cf999091409bda244
Somelier4ik <almizvitalij@gmail.com>
2021-04-04 00:03:20 +0300

    Добавляет стили планшетной версии

--- js/index.js
+++ js/index.js
@@ -1,10 +1,28 @@
 const burger = document.querySelector("[data-burger]");
 const dropdown = document.querySelector("[data-dropdown]");
+const body = document.querySelector("[data-no-scroll]");
 
 burger.addEventListener("click", toggleClassBurger);
 
 function toggleClassBurger() {
   dropdown.classList.toggle("shown");
-
   burger.classList.toggle("close");
+  body.classList.toggle("no-scroll");
 }
+
+const anchors = document.querySelectorAll('a[href^="#"]');
+
+anchors.forEach((anchor) => {
+  anchor.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const goTo = anchor.hasAttribute("href")
+      ? anchor.getAttribute("href")
+      : "body";
+
+    document.querySelector(goTo).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+});
